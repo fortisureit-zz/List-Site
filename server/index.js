b839faa80ca6230411b44e3d21233a36adfc8eeb
Alex <ajbarbati@gmail.com>
2020-03-20 12:05:12 -0400

    changed to a pool request

--- server/index.js
+++ server/index.js
@@ -8,7 +8,15 @@ const app = express()
 
 const SELECT_ALL_PRODUCTS_QUERY = 'SELECT * FROM Restrnts'
 
-const connection = mysql.createConnection('mysql://doadmin:xwuk8weoo3swlnbd@private-restaurant-list-db-do-user-7144326-0.a.db.ondigitalocean.com:25060/defaultdb?ssl-mode=REQUIRED')
+const pool = mysql.createPool({
+    connectionLimit : 10,
+    host            : 'restaurant-list-db-do-user-7144326-0.a.db.ondigitalocean.com',
+    user            : 'doadmin',
+    password        : 'xwuk8weoo3swlnbd',
+    database        : 'defaultdb'
+
+})
+    
 
 app.use(cors())
 
@@ -19,7 +27,7 @@ app.get('/', (req, res) => {
 
 app.get('/restaurants', (req,res) => {
 
-    connection.query(SELECT_ALL_PRODUCTS_QUERY, (err, results) => {
+    pool.query(SELECT_ALL_PRODUCTS_QUERY, (err, results) => {
         if(err) {
             return res.send(err)
         } else {
