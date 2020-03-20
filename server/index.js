ef11532130ca71cc06fce48ec3deef3591c25145
Alex <ajbarbati@gmail.com>
2020-03-20 11:09:24 -0400

    create react app

--- server/index.js
+++ server/index.js
@@ -2,21 +2,13 @@ const express = require('express')
 const cors = require('cors')
 const mysql = require('mysql')
 
+
+
 const app = express()
 
-const SELECT_ALL_PRODUCTS_QUERY = 'SELECT * FROM users'
+const SELECT_ALL_PRODUCTS_QUERY = 'SELECT * FROM Restrnts'
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'sys'
-})
-    connection.connect(err => {
-        if(err) {
-            return err
-        }
-    })
+const connection = mysql.createConnection('mysql://doadmin:xwuk8weoo3swlnbd@private-restaurant-list-db-do-user-7144326-0.a.db.ondigitalocean.com:25060/defaultdb?ssl-mode=REQUIRED')
 
 app.use(cors())
 
@@ -31,7 +23,7 @@ app.get('/restaurants', (req,res) => {
         if(err) {
             return res.send(err)
         } else {
-            return res.json({
+            return res.send({
                 data: results
             })
         }
