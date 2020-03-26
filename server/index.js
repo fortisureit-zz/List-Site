3f554c6cdcc4ad954081dc0e2bdad99ccfd4bfed
Alex <ajbarbati@gmail.com>
2020-03-26 11:58:24 -0400

    using old cors setup

--- server/index.js
+++ server/index.js
@@ -30,18 +30,18 @@ const pool = mysql.createPool({
     }
 
 })
-const whitelist = ['http://localhost:3000', 'http://localhost:5000/server', 'http://localhost:5000']
-const corsOptionsDelegate = function (req, callback) {
-  const corsOptions = {}
-  if (whitelist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
+// const whitelist = ['http://localhost:3000', 'http://localhost:5000/server', 'http://localhost:5000']
+// const corsOptionsDelegate = function (req, callback) {
+//   const corsOptions = {}
+//   if (whitelist.indexOf(req.header('Origin')) !== -1) {
+//     corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+//   } else {
+//     corsOptions = { origin: false } // disable CORS for this request
+//   }
+//   callback(null, corsOptions) // callback expects two parameters: error and options
+// }
 
-app.use(cors(corsOptionsDelegate))
+app.use(cors())
 
 // app.get('/', (req, res) => {
 //     res.send("Index")
